cf6f89fa19bb961765617e48b1efd99c6039a68d
DarshanNagesh10 <darshann.801f@gmail.com>
2026-05-31 09:28:20 +0530

    Redux thunk

--- Redux/app/src/App.jsx
+++ Redux/app/src/App.jsx
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './store'
 import Account from './Account'
 import Form from './Form'
+import UserCom from './UserCom'
 function App() {
   return (
     <div className="container text-center mt-5">
-      <Form />
-      <Account />
+      {/* <Form />
+      <Account /> */}
+      <UserCom />
     </div>
   )
 }
--- Redux/app/src/main.jsx
+++ Redux/app/src/main.jsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import store from "./store.js";
+// import store from "./store.js";
+import store_async from "./store_async.js";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
+    {/* <Provider store={store}> */}
+    <Provider store={store_async}>
       <App />
     </Provider>
   </StrictMode>,
